b873cea30ce7e04d829d11443c3e9e93101f1ccb
Himanshu Trehan <88142817+himCustomerGlu@users.noreply.github.com>
2024-09-25 10:17:36 +0530

    Callback Handler Example added in code

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import CustomerGluComponent from "./component/CustomerGluComponent";
 import { Route, Routes } from "react-router-dom";
 import Cgtest from "./component/Cgtest";
 import Error from "./component/Error";
+import { useEffect } from "react";
 
 function Main() {
+  return(
   <div className="App">
     <div
       id="embedId"
@@ -19,43 +21,91 @@ function Main() {
       }}
     >
       <CustomerGluComponent
-        gluToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJnbHV0ZXN0LTI1OCIsImdsdUlkIjoiNmUwZTUwZDItMzQ1Mi00YTE1LTljMTMtM2YzZTJmZDA2NGQ3IiwiY2xpZW50IjoiN2IxOTEwYTQtNzMxYS00MmY2LTkyODQtMmU2NGRhNGM5MTRkIiwiZGV2aWNlSWQiOiJnbHV0ZXN0LTI1OF9kZWZhdWx0IiwiZGV2aWNlVHlwZSI6ImRlZmF1bHQiLCJpc0xvZ2dlZEluIjp0cnVlLCJpYXQiOjE3MjQ3NDEwOTksImV4cCI6MTc1NjI3NzA5OX0.q4CnB-foMnkVsQQgm7ijK8YcxskDLENDNTkwtgrZztQ"
-        userId="glutest-258"
-        region="us"
-      >
+          gluToken=""
+          userId=""
+          region="in"
+        >
         <div></div>
       </CustomerGluComponent>
     </div>
-  </div>;
+
+  </div>
+  );
 }
 
+
+// Attach the event listener using the stored reference
+
+
+
 export default function App() {
+
+  const messageHandler = (event) => {
+    try {
+    //  console.log(event);
+      // Parse the received message
+      if (event.origin !== "https://constellation.customerglu.com") {
+        return;
+      }
+      let eventData;
+  
+      // Check if event.data is a string or an object
+      if (typeof event.data === 'string') {
+        // Try to parse the string as JSON
+     
+        eventData = JSON.parse(event.data);
+      } else{
+        return
+      }
+     // const eventData = JSON.parse(event.data);
+  
+      // Process the event data
+      if (eventData.eventName) {
+        // Handle different event types
+        switch (eventData.eventName) {
+          case "CLOSE":
+            // Handle your close logic
+            console.log( "eventData");
+
+            console.log( eventData);
+            break;
+            case "OPEN_DEEPLINK":
+             // Handle your Navigation logic
+              console.log( "eventData");
+            console.log( eventData);
+            break;
+            
+      }
+  
+      }
+    } catch (error) {
+      console.error("Error parsing event data:", error);
+    }
+  };
+
+
+  useEffect(() => {
+    // Attach the event listener
+    window.addEventListener("message", messageHandler, false);
+
+    // Cleanup function to deregister the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("message", messageHandler, false);
+    };
+  }, []); 
+
+
+ 
+  
+
   return (
-    // <div className="App">
-    //   <div
-    //     id="embedId"
-    //     style={{
-    //       display: "flex",
-    //       alignItems: "center",
-    //       justifyContent: "center",
-    //       cursor: "pointer",
-    //     }}
-    //   >
-    //     <CustomerGluComponent
-    //       gluToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJnbHV0ZXN0LTI1OCIsImdsdUlkIjoiNmUwZTUwZDItMzQ1Mi00YTE1LTljMTMtM2YzZTJmZDA2NGQ3IiwiY2xpZW50IjoiN2IxOTEwYTQtNzMxYS00MmY2LTkyODQtMmU2NGRhNGM5MTRkIiwiZGV2aWNlSWQiOiJnbHV0ZXN0LTI1OF9kZWZhdWx0IiwiZGV2aWNlVHlwZSI6ImRlZmF1bHQiLCJpc0xvZ2dlZEluIjp0cnVlLCJpYXQiOjE3MjQ3NDEwOTksImV4cCI6MTc1NjI3NzA5OX0.q4CnB-foMnkVsQQgm7ijK8YcxskDLENDNTkwtgrZztQ"
-    //       userId="glutest-258"
-    //       region="us"
-    //     >
-    //       <div></div>
-    //     </CustomerGluComponent>
-    //   </div>
-    // </div>
-    <>
+
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/test" element={<Cgtest />} />
         <Route path="*" element={<Error /> } />
       </Routes>
-    </>
+    
   );
 }
+
