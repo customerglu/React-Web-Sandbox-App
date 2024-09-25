51aa41b4a8698f10e67992c0a4c2ea2856e87fd3
Prateek Gupta <prateek@customerglu.net>
2024-09-26 01:12:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-// import {CustomerGluComponent} from "@customerglu/react-web-sdk";
+import {CustomerGluComponent} from "@customerglu/react-web-sdk";
 import "./styles.css";
-// import { CustomerGluComponent } from "@customerglu/react-web-sdk";
 
-import CustomerGluComponent from "./component/CustomerGluComponent";
-import { Route, Routes } from "react-router-dom";
+import {
+    Route,
+    Routes
+} from "react-router-dom";
 import Cgtest from "./component/Cgtest";
 import Error from "./component/Error";
-import { useEffect } from "react";
+import {
+    useEffect
+} from "react";
 
 function Main() {
-  return(
-  <div className="App">
-    <div
-      id="embedId"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-      }}
-    >
-      <CustomerGluComponent
-          gluToken=""
-          userId=""
-          region="in"
-        >
-        <div></div>
-      </CustomerGluComponent>
-    </div>
+    return ( <
+        div className = "App" >
+        <
+        div id = "embedId"
+        style = {
+            {
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                cursor: "pointer",
+            }
+        } >
+        <
+        CustomerGluComponent gluToken = ""
+        userId = ""
+        region = "in" >
+        <
+        div > < /div> <
+        /CustomerGluComponent> <
+        /div>
 
-  </div>
-  );
+        <
+        /div>
+    );
 }
 
 
@@ -40,72 +45,76 @@ function Main() {
 
 export default function App() {
 
-  const messageHandler = (event) => {
-    try {
-    //  console.log(event);
-      // Parse the received message
-      if (event.origin !== "https://constellation.customerglu.com") {
-        return;
-      }
-      let eventData;
-  
-      // Check if event.data is a string or an object
-      if (typeof event.data === 'string') {
-        // Try to parse the string as JSON
-     
-        eventData = JSON.parse(event.data);
-      } else{
-        return
-      }
-     // const eventData = JSON.parse(event.data);
-  
-      // Process the event data
-      if (eventData.eventName) {
-        // Handle different event types
-        switch (eventData.eventName) {
-          case "CLOSE":
-            // Handle your close logic
-            console.log( "eventData");
+    const messageHandler = (event) => {
+        try {
+            // Parse the received message
+            if (event.origin !== "https://constellation-us.customerglu.com") {
+                return;
+            }
+            let eventData;
 
-            console.log( eventData);
-            break;
-            case "OPEN_DEEPLINK":
-             // Handle your Navigation logic
-              console.log( "eventData");
-            console.log( eventData);
-            break;
-            
-      }
-  
-      }
-    } catch (error) {
-      console.error("Error parsing event data:", error);
-    }
-  };
+            // Check if event.data is a string or an object
+            if (typeof event.data === 'string') {
+                // Try to parse the string as JSON
 
+                eventData = JSON.parse(event.data);
+            } else {
+                return
+            }
+            // const eventData = JSON.parse(event.data);
 
-  useEffect(() => {
-    // Attach the event listener
-    window.addEventListener("message", messageHandler, false);
+            // Process the event data
+            if (eventData.eventName) {
+                // Handle different event types
+                switch (eventData.eventName) {
+                    case "OPEN_DEEPLINK":
+                        // Handle your Navigation logic
+                        console.log("eventData", eventData);
+                        break;
 
-    // Cleanup function to deregister the event listener when the component is unmounted
-    return () => {
-      window.removeEventListener("message", messageHandler, false);
+                }
+
+            }
+        } catch (error) {
+            console.error("Error parsing event data:", error);
+        }
     };
-  }, []); 
 
 
- 
-  
+    useEffect(() => {
+        // Attach the event listener
+        window.addEventListener("message", messageHandler, false);
 
-  return (
+        // Cleanup function to deregister the event listener when the component is unmounted
+        return () => {
+            window.removeEventListener("message", messageHandler, false);
+        };
+    }, []);
 
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/test" element={<Cgtest />} />
-        <Route path="*" element={<Error /> } />
-      </Routes>
-    
-  );
+
+
+
+    return (
+
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = {
+            < Main / >
+        }
+        /> <
+        Route path = "/test"
+        element = {
+            < Cgtest / >
+        }
+        /> <
+        Route path = "*"
+        element = {
+            < Error / >
+        }
+        /> <
+        /Routes>
+
+    );
 }
-
